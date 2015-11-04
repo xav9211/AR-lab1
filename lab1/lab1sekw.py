6c15892fb83ca4ddc8adbdfea4c6f8893e4f669f
xav9211 <ksawery.glaz@gmail.com>
2015-11-04 21:42:30 +0100

    heatmap

--- lab1/lab1sekw.py
+++ lab1/lab1sekw.py
@@ -1,10 +1,7 @@
 __author__ = 'xa92'
 import copy
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
-import matplotlib.pyplot as plt
-from matplotlib.mlab import griddata
-import numpy as np
+import pygmyplot as pyg
+import Tkinter as TK
 
 def initialize(up, left, bottom, right, mid, size):
     result = [[mid]*size for _ in range(size)]
@@ -34,59 +31,34 @@ def show(plate, size):
         print row + "\n"
 
 def visualize(plate, size):
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
 
     result = []
     py = []
+
     for i in range(1, size - 1):
         result.extend(plate[i][1:size - 1])
-        py.extend([i]*(size-2))
+        for j in range(1, size - 1):
+            py.append((i, j))
 
-    x = np.array(range(1, size - 1)*(size-2))
-    y = np.array(py)
-    z = np.array(result)
+    tk = TK.Tk()
+    hm = pyg.xy_heat(py, result, master=tk)
+    TK.Button(tk, text="Quit", command=tk.destroy).pack()
+    tk.mainloop()
 
-    xi = np.linspace(min(x), max(x))
-    yi = np.linspace(min(y), max(y))
-
-    X, Y = np.meshgrid(xi, yi)
-    Z = griddata(x, y, z, xi, yi)
-    # print(X)
-
-    surf = ax.plot_surface(Y, X, Z, rstride=1, cstride=1, cmap=cm.jet,
-                       linewidth=1, antialiased=True)
-
-    ax.set_zlim3d(np.min(z), np.max(z))
-    fig.colorbar(surf)
-
-    plt.show()
-
-def compute(plate, size, err):
-    while True:
+def compute(plate, size, iter):
+    for i in xrange(iter):
         prev = copy.deepcopy(plate)
         for i in range(1, size - 1):
             for j in range(1, size - 1):
-                plate[i][j] = computePoint(prev[i-1][j], prev[i][j-1], prev[i+1][j], prev[i][j+1])
-        if not checkErr(prev, plate, size, err):
-            break
+                plate[i][j] = computePoint(prev[i-1][j], prev[i][j-1], prev[i+1][j], prev[i][j+1], prev[i][j])
 
-def computePoint(yp, xp, yn, xn):
+def computePoint(yp, xp, yn, xn, xy):
     return (yp + xp + yn + xn)/4
-
-def checkErr(prev, plate, size, err):
-    for i in range(1, size - 1):
-        for j in range(1, size - 1):
-            absDif = abs(prev[i][j] - plate[i][j])
-            if absDif > err:
-                return True
-    return False
-
 
 def main():
     size = 30
-    plate = initialize(100, 100, 0, 0, 0, size)
-    compute(plate, size, 1)
+    plate = initialize(100.0, 0, 0, 0, 0, size)
+    compute(plate, size, 500)
     show(plate, size)
     visualize(plate, size)
 
